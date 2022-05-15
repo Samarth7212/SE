a1bbdd2a098362c63783ba116fed754e9923ee18
Samarth7212 <samarthkamble7212@gmail.com>
2022-05-16 00:20:00 +0530

    .

--- Sem-4/MPL/assignment7.asm
+++ Sem-4/MPL/assignment7.asm
@@ -1,113 +1,134 @@
-%macro IO 4
-mov rax, %1
-mov rdi, %2
-mov rsi, %3
-mov rdx, %4
+section .data
+arr1 db 11h,22h,33h,44h,55h
+space db ' '
+newLine db 10
+msg1: db 'Before: ',0xa
+len1: equ $-msg1
+msg2: db 'After: ',0xa
+len2: equ $-msg2
+
+section .bss
+adress resb 20
+data resb 5
+dig_count resb 2
+count resb 2
+arr2 resb 10
+
+%macro print 2
+mov rax,01
+mov rdi,01
+mov rsi,%1
+mov rdx,%2
 syscall
 %endmacro
 
-section .data
-	space db " "
-
-	before_msg db	10,"Before Transfer: "
-	before_len	equ	$-before_msg
-
-	after__transfer db	10,"After Transfer: "
-	after__transfer_len	equ	$-after__transfer
-	
-	source_block_msg db	10,"Source Block: "
-	source_block_len equ	$-source_block_msg
-
-	destination_block_msg db	10,"Destination Block: "
-	destination_len	equ	$-destination_block_msg
-
-	source_block db	5h,4h,3h,2h,1h
-	destination_block times	5 db 0
-	
-section .bss
-	Result resB 2
-
 section .text
-	global _start
+global _start
 
 _start:
-	IO 01, 01, before_msg, before_len
+;Block transfer using string operation
+    mov esi,arr1
+    mov edi,arr2
+    xor rcx,rcx
+    mov cx,05
 
-	IO 01, 01,  source_block_msg, source_block_len
-	mov	rsi,source_block
-	call disp_block
-
-	IO 01, 01, destination_block_msg, destination_len
-	mov	rsi,destination_block
-	call disp_block
-	
-	call Block_Transfer
-
-	IO 01, 01, after__transfer, after__transfer_len 
-
-	IO 01, 01,  source_block_msg, source_block_len
-	mov	rsi,source_block
-	call disp_block
-
-	IO 01, 01, destination_block_msg, destination_len
-	mov	rsi,destination_block
-	call disp_block
-
-    MOV	RAX,60
-    MOV	RDI,0
+    cld ;clear direction flag for autoincrement of si and di
+    rep movsb
+    
+    ;logic to print adress and data
+    ;before block transfer
+    print msg1,len1
+    
+    mov byte[count],05
+    mov rsi,arr1
+    ;call procedure print adress to print the 64- bit adress which is in rbx
+    
+    up1:
+        mov rbx ,rsi
+    push rsi
+    call printadd
+    print space,01
+    pop rsi
+    
+    ;call procedure print data to print 8-bit data which is in register dl
+    mov dl,[rsi]
+    push rsi
+    call printdata 
+    print newLine,01
+    
+    pop rsi
+    inc rsi
+    dec byte[count]
+    jnz up1
+    ;After block transfer
+    
+    print msg2,len2
+    mov byte[count],05
+    mov rsi,arr2
+    ;call procedure print adress to print the 64- bit adress which is in rbx
+    
+    up3: 
+        mov rbx ,rsi
+    push rsi
+    call printadd
+    print space,01
+    pop rsi
+    ;call procedure print data to print 8-bit data which is in register dl
+    mov dl,[rsi]
+    push rsi
+    call printdata
+    print newLine,01
+    pop rsi
+    inc rsi
+    dec byte[count]
+    jnz up3
+    mov rax,60
+    mov rdi,00
     syscall
+    
+printadd:
+    mov byte[dig_count],16
+    mov rsi,adress
+    up:
+    rol rbx,04
+    mov al,bl
+    and al,0Fh
+    cmp al,09
+    jbe l1
+    add al,07h
+    l1:
+        add al,30h
+    mov [rsi], al ;mov leng to str_len variable for printing
+    inc rsi
+    dec byte[dig_count]
+    jnz up
 
-
-HexToASCII:
-	MOV	RSI,Result+1
-	MOV	RCX,2
-	MOV	RBX,16
-
-    next_digit:
-	    XOR	RDX,RDX
-	    DIV	RBX
-	    cmp	DL,9	
-	    jbe	add30
-	    add	DL,07H
-
-    add30:
-	    add DL,30H
-	    mov	[RSI],DL
-
-	    dec RSI
-	    dec	RCX
-	    jnz	next_digit
-
-	    IO 01, 01, Result, 2
+    mov rax,01
+    mov rdi,01
+    mov rsi,adress
+    mov rdx,16
+    syscall
     ret
 
-Block_Transfer:
-	mov	rsi,source_block
-	mov	rdi,destination_block
-	mov	rcx,5
-
-    back:	
-        mov	al,[rsi] 
-	    mov	[rdi],al
-
-	    inc	rsi
-	    inc	rdi
-
-	    dec	rcx
-	    jnz	back
-    ret
-disp_block:
-	mov	rbp,5
-    next_num:
-	    mov	al,[rsi]
-	    push rsi
-
-	    call HexToASCII
-	    IO 01, 01, space, 1
-	
-	    pop	rsi
-	    inc	rsi
-	
-	    dec	rbp
-	    jnz	next_num
+    printdata:
+        mov byte[dig_count],02
+        mov rsi,data
+        up2:
+            rol dl,04
+            mov al,dl
+            and al,0Fh
+            cmp al,09
+            jbe l2
+            add al,07h
+            l2:
+                add al,30h
+            mov [rsi], al ;mov leng to str_len variable for printing
+            inc rsi
+            dec byte[dig_count]
+            jnz up2
+        mov rax,01
+        mov rdi,01
+        mov rsi,data
+        mov rdx,02
+        syscall
     ret
